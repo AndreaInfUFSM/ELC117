e1b69d5079c49002afb7d57ef8de255920fb668c
Eduardo Tiago Braun <eduardo.braun@ecomp.ufsm.br>
2017-03-19 21:07:49 -0300

    Adiciona numeração aos exercícios de t1p2

--- t1/t1parte2.py
+++ t1/t1parte2.py
@@ -22,7 +22,7 @@
 # THE SOFTWARE.
 from datetime import date
 
-# Escreva uma função que receba uma lista de nomes e retorne uma lista com a
+# 1. Escreva uma função que receba uma lista de nomes e retorne uma lista com a
 # string "Sr. " adicionada ao início de cada nome.
 def srify(l):
     """Recebe uma lista de nomes e adiciona a string "Sr. " no inicio de cada
@@ -35,7 +35,7 @@ def srify(l):
     out: list"""
     return list(map(lambda s:"Sr. " + s, l))
 
-# Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1
+# 2. Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1
 # para cada número n da lista.
 def calcListPoly(l):
     """Recebe uma lista de números aplicando a função `calcPoly` para cada
@@ -48,7 +48,7 @@ def calcListPoly(l):
     out: list"""
     return list(map(lambda n:(3.*n**2.)+(2./n)+1., l))
 
-# Crie uma função que receba uma lista de nomes e retorne outra lista com
+# 3. Crie uma função que receba uma lista de nomes e retorne outra lista com
 # somente aqueles nomes que terminarem com a letra 'a'.
 def endsWithA(l):
     """Recebe uma lista de strings. Retorna uma lista contendo somente as
@@ -61,7 +61,7 @@ def endsWithA(l):
     out: list"""
     return list(filter(lambda s:s[-1]=="a", l))
 
-# Escreva uma função que, dada uma lista de idades de pessoas no ano atual,
+# 4. Escreva uma função que, dada uma lista de idades de pessoas no ano atual,
 # retorne uma lista somente com as idades de quem nasceu depois de 1970. Para
 # testar a condição, sua função deverá subtrair a idade do ano atual. Exemplo
 # de uso:
@@ -79,7 +79,7 @@ def idades(l):
     out: list"""
     return list(filter(lambda n:date.today().year-n>1970, l))
 
-# O código abaixo é escrito em Python imperativo. Escreva um código equivalente
+# 5. O código abaixo é escrito em Python imperativo. Escreva um código equivalente
 # usando programação funcional.
 #
 # numbers = [1, 2, 3, 4]
